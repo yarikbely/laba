f586002bfe14b2c876b002ac453fb7ebce476b35
novilaa <159593281+novilaa@users.noreply.github.com>
2024-02-20 22:13:29 +0200

    Add files via upload

---
+++ CMake2/laba_2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main(){
+	setlocale(LC_ALL, "RU");
+	//unsigned int x = 7; //7=00000111
+	unsigned int x = 98;//98=01100010
+	// unsigned int x = 241;//241=11110001
+	int i;
+	int y = 1;
+	std::cout << "Введите какой бит числа вы хотите узнать: ";
+	std::cin >> i;
+	x = (x << 7-i );
+	x = (x >> );
+	std::cout << "Он равен: " << x;
+
+	return 0;
+  
+}
+
